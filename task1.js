4e2180d3e23f29543f937d4120a47b27af66c3bb
VISHNU D <umavishnu005@gmail.com>
2026-05-19 09:14:57 +0400

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,278 @@
+//1. Create a variable using vars and store your name.
+        var name = "VISHNU D"
+        
+// 2. Print your name using console.log().
+        console.log(name);
+
+
+// 3. Create a variable using let and store your age.
+        let age = 21
+
+
+// 4. Change the value of age and print it.
+        age = 22
+        console.log(age);
+
+
+// 5. Create a constant variable for country name.
+        const country1 = "India"
+        
+// 6. Print the country name.
+        console.log(country1)
+
+
+// 7. Try redeclaring var variable and observe output.
+        var num1 = 33
+        var num1 = 44
+        console.log(num1);
+        //ans: 44
+//8. Try redeclaring let variable and observe error
+        let a1 = 26
+        let a1 = 34
+        console.log(a1);
+        ans: syntax error
+
+// 9. Try reassigning const variable and observe error.
+        const b1 = 6
+        b1 = 10
+        console.log(b1)
+        ans: syntax error
+
+
+// 10. Write difference between var and let.
+         var can be  declared,reassigned and redeclared,but in let 
+         can be only declared and reassigned cannot be redeclared.
+
+
+// 11. Write difference between let and const.
+        let can be declared and reassigned.
+        const can be declared only.
+
+
+
+// 12. Predict output
+
+var a = 10
+a = 20
+var a = 30
+
+console.log(a)
+
+// answer : 30
+
+// 13. Predict output
+
+let b = 50
+b = 60
+
+console.log(b)
+answer : 60
+
+
+
+// 14. Predict output
+
+const c = 100
+
+console.log(c)
+answer : 100
+
+
+// 15. Use console.log() to print "Hello World".
+        console.log("Hello World")
+
+
+// 16. Use alert() to show "Welcome User".
+       alert("Welcome User");
+
+
+// 17. Use confirm() to ask user:
+// "Do you like JavaScript?"
+        confirm( "Do you like JavaScript?")
+
+// 18. Use prompt() to ask user name.
+       let Username=  prompt ("enter your name");
+
+
+// 19. Print prompt value in console.
+        console.log(Username)
+        
+// 20. Use document.writeln() to print "Batch 33".
+        document.writeln("Batch 33")
+
+
+// 21. Create variable studentName and store your name.
+        var studentName = "Dharshini Priya M"
+
+
+// 22. Create variable studentAge and store your age.
+        var studentAge = 23
+
+
+// 23. Print both variables together.
+        console.log( studentName,studentAge)
+
+
+// 24. Create a variable for mobile number.
+        let mobileNumber = 9787471190;
+
+
+// 25. Print mobile number using console.log().
+
+    console.log(mobileNumber);
+
+
+// 26. Create variable using var for favorite color.
+        var color = "red"
+
+
+// 27. Reassign another color value.
+        color = "blue"
+
+
+// 28. Create variable using let for marks.
+        let mark = 60
+
+
+// 29. Change marks value and print it.
+        mark = 95
+
+
+// 30. Create const variable for PI value.
+        const PI = 3.14
+
+
+// 31. Print PI value.
+        console.log(PI);
+
+
+// 32. Predict output
+
+var x = 5
+var x = 15
+
+console.log(x)
+
+15
+
+
+// 33. Predict output
+
+let y = 25
+y = 35
+
+console.log(y)
+
+35
+
+
+// 34. Find error
+
+const z = 40
+z = 45
+
+console.log(z)
+
+syntaxerror
+
+
+// 35. Create alert box with your college name.
+
+alert("SRI KRISHNA ARTS AND SCIENCE College");
+
+
+// 36. Ask user age using prompt().
+
+   let UserAge = prompt("enter your age");
+
+
+// 37. Print user age in console.   
+        console.log(UserAge)
+
+
+// 38. Ask user confirmation:
+// "Did you complete HTML?"
+        let userCompletion = confirm("did you complete html")
+
+
+// 39. Print confirm result in console.
+        console.log(userCompletion);
+
+
+// 40. Print "JavaScript Class" using document.writeln().
+        document.writeln("JavaScript Class");
+
+
+// 41. Create 3 variables:
+// city, state, country.    
+        let city = "MADURAI";
+        let state = "Tamil Nadu";
+        let country = "India";
+
+
+
+// 42. Print all variables together.
+        console.log(city,state,country);
+
+
+// 43. Create one example for var.
+        var x1 = 15;
+        x1 =20;
+        var x1 =25;
+        console.log(x1);
+
+
+// 44. Create one example for let.
+        let pet = "dog"
+        pet = "cat"
+        console.log(pet);
+
+// 45. Create one example for const.
+        const BG = "B+ve"
+        console.log(BG);
+
+
+
+// 46. Predict output
+
+var num = 100
+num = 200
+
+console.log(num)
+
+200
+
+
+// 47. Predict output
+
+let price = 500
+price = 700
+
+console.log(price)
+
+700
+
+
+// 48. Predict output
+
+const password = 12345
+
+console.log(password)
+
+12345
+
+
+// 49. Write rules of var.
+        var can be declared.
+        var can be reassigned.
+        var can be redeclared.
+
+
+// 50. Write rules of let and const.
+        let can be declared.
+        let can be reassigned.
+        let cannot be redeclared.
+
+        const must be initialized while declaring.
+        const cannot be reassigned.
+        const cannot be redeclared.
---
+++ task2.js
@@ -0,0 +1,210 @@
+// 1. Create a variable and store your favorite food.
+
+let Food = "Biriyani"
+console.log(Food);
+
+// 2. Create a variable and store your mobile number.
+
+let MobileNum = 9787471190
+console.log(MobileNum);
+
+// 3. Create a boolean variable with false value.
+
+let a = false
+console.log(a)
+
+// 4. Create one undefined variable and print typeof.
+
+var c;
+console.log(typeof c);
+
+// 5. Create one array with 6 animal names.
+
+let animals = ["Dog", "Cat","Cow","Lion","Tiger","Deer"]
+
+// 6. Print second animal from array.
+
+console.log(animals[1]);
+
+// 7. Print last animal from array using length.
+
+console.log(animals.length-1)
+
+// 8. Create object with bike details (name, model, color).
+
+let bike = {
+    name: "DUKE", 
+    model : "390",
+    color : "WHITE",
+} 
+console.log(bike);
+
+// 9. Print bike color from object.
+
+console.log(bike.color);
+
+// 10. Create two variables and subtract values.
+
+let x1 = 10
+let x2 = 4
+console.log(x1-x2);
+
+// 11. Create two variables and divide values.
+
+let x = 10
+let y = 2
+console.log(x/y);
+
+
+// 12. Find 15 % 2.
+
+console.log(15%2);
+
+// 13. Find 2 ** 5.
+
+console.log(2**5);
+
+// 14. Create variable with 5 and use post increment.
+
+let a1 = 5;
+console.log(a1++);
+
+
+// 15. Create variable with 10 and use pre increment.
+
+let a2 = 10;
+console.log(++a2);
+
+// 16. Create variable with 20 and use post decrement.
+
+let a3 = 20;
+console.log(a3--);
+
+// 17. Create variable with 50 and use pre decrement.
+
+let a4 = 50;
+console.log(--a4);
+
+// 18. Check 100 > 50.
+
+console.log(100>50) // true
+
+
+// 19. Check 25 < 10.
+
+console.log(25<10) // false
+
+// 20. Check 50 >= 50.
+
+console.log(50 >= 50)  // true
+
+// 21. Check 30 <= 25.
+
+console.log(30 <= 25)  // false
+
+// 22. Check 10 == "10".
+
+console.log (10 == "10") // true
+
+// 23. Check 10 === "10".
+
+console.log (10 === "10") // false
+
+// 24. Check 25 != "25".
+
+console.log (25 != "25") //false
+
+// 25. Check 25 !== "25".
+
+console.log (25 !== "25") //true
+
+// 26. Use AND operator:
+//     10 > 5 && 20 > 15
+
+console.log (10 > 5 && 20 > 15)  // true
+
+// 27. Use AND operator:
+//     5 > 10 && 20 > 15
+
+console.log (5 > 10 && 20 > 15)   // false
+
+// 28. Use OR operator:
+//     5 > 10 || 20 > 15
+
+console.log (5 > 10 || 20 > 15)  //true
+
+// 29. Use OR operator:
+//     2 > 5 || 1 > 10
+
+console.log (2 > 5 || 1 > 10)  //false
+
+// 30. Use NOT operator:
+//     !(10 > 5)
+
+console.log (!(10>5))  // false
+
+// 31. Use NOT operator:
+//     !(5 > 10)
+
+console.log(!(5 > 10))  // true
+
+// 32. Create ternary operator:
+//     if 50 > 25 print "True"
+//     else print "False"
+
+console.log(50 > 25 ? "True" : "False"); //True
+
+// 33. Create ternary operator:
+//     if 18 >= 21 print "Adult"
+//     else print "Minor"
+
+console.log(18 >= 21 ? "Adult" : "Minor");  //Minor
+
+// 34. Create one array with 5 city names and print third city.
+
+let City = ["Trichy","Chennai", "Madurai" ,"Coimbatore" , "Erode"]
+console.log(City[2]);
+
+// 35. Create one object with employee details and print employee name.
+
+var employee = {
+    name: "VISHNU",
+    id: 101,
+    role: "Full Stack Developer"
+};
+
+console.log(employee.name);
+
+
+// 36. Create variable with string "100" and print typeof.
+
+let num = "100"
+console.log(typeof(num));
+
+// 37. Create variable with number 100 and print typeof.
+
+let num1 = 100
+console.log(typeof(num1));
+
+
+// 38. Create one null variable and print it.
+
+var d = null;
+console.log(d);
+
+
+// 39. Create one array with mixed datatypes.
+
+let data = ["VISHNU", 21, true, null];
+console.log(data);
+
+// 40. Create object with student details:
+//     name, mark, passed(boolean)
+
+var student = {
+    name: "VISHNU D",
+    mark: 80,
+    passed: true
+};
+
+console.log(student);
